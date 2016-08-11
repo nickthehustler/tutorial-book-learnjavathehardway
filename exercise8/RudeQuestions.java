ef1704c61b0232ea2f2cf6909c0082737cf3e0b6
Nick Brown <nicholas.g.brown@gmail.com>
2016-08-11 13:49:05 -0400

    Completed exercise 8.

--- exercise8/RudeQuestions.java
+++ exercise8/RudeQuestions.java
@@ -27,5 +27,31 @@ public class RudeQuestions {
     System.out.print("Well, thanks for answering my rude questions, ");
     System.out.println(name + ".");
 
+    /* Does the program blow up if you enter an integer value
+    when it is expecting you to type a double? Put an answer
+    in a comment at the bottom of the code, along with your guess why or why not.*/
+    /* Integers won't blow up the program. The Integer value gets
+    converted to a Double value. */
+
+    /* Does the program blow up if you enter a numeric value (integer or double)
+    when it is expecting a String? Put an answer in a comment
+    at the bottom of the code, along with your guess why or why not. */
+    /* No it won't blow up. Numeric values get interpreted as strings. */
+
+    /* Type something that makes the program blow up on every question possible,
+    and add comments explaining what blew it up and why. */
+    /* I used '!' to blow up any question that involes Integer-typed or Double-typed variables.
+    I couldn't get the String variable to blow up. */
+
+    /* Add a new variable of your choosing. Add another question.
+    (It doesn’t have to be rude.) Let the human put an answer to your question
+    into your new variable, and display the human’s answer on the screen afterward. */
+
+    Double favoriteNumber;
+
+    System.out.println("Before you leave what is your favorite number?");
+    favoriteNumber = keyboard.nextDouble();
+    System.out.println(favoriteNumber + " is your favorite number!? You are something else!");
+
   }
 }
