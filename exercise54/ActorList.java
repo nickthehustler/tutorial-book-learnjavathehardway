9f102f3b2d148b1c2d6954fc01589149fb008b4e
Nick Brown <nicholas.g.brown@gmail.com>
2016-08-30 18:33:35 -0400

    Completed exercise 54.

---
+++ exercise54/ActorList.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Actor {
+  String name;
+  String role;
+  String birthdate;
+}
+
+public class ActorList {
+  public static void main(String[] args) throws Exception {
+    String url = "https://learnjavathehardway.org/txt/s01e01-cast.txt";
+    // Scanner inFile = new Scanner((new java.io.File("s01e01-cast.txt"));
+    Scanner inFile = new Scanner((new java.net.URL(url)).openStream());
+    int counter = 0;
+
+    while(inFile.hasNext()) {
+      Actor a = getActor(inFile);
+      System.out.print(a.name + " was born on " + a.birthdate);
+      System.out.println(" and played " + a.role);
+      counter++;
+    }
+    inFile.close();
+    System.out.println(counter + " records were processed.");
+  }
+
+  public static Actor getActor(Scanner input) {
+    Actor a = new Actor();
+    a.name = input.nextLine();
+    a.role = input.nextLine();
+    a.birthdate = input.nextLine();
+
+    return a;
+  }
+}
